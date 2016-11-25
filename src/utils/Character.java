c78771177684f3bd4fef1c83d70e4d600e7c885b
Hugo Illous <hugo.illous@cea.fr>
2016-11-25 16:33:05 +0100

    add height points

--- src/utils/Character.java
+++ src/utils/Character.java
@@ -2,29 +2,33 @@ package utils;
 import java.util.HashMap;
 
 public class Character {
+    
+    public String name;
+    
+    // 1. Basic informations
+    private int level;
+    private int px;
+    private int hp;
+    
+    private Race race;
+    private Class cl;
+    private String gender; 
+    
+	
+    
+    // 2. Physical informations
+    private int size;
+    private int weight;
+    
+    // 3. Characteristics. 
+	
+    private HashMap<Characteristic,Integer> characteristics; 
+    private Skill[] formations;
+	
 
-	public String name;
-	
-	// 1. Basic informations
-	private int level;
-	private int px;
-	
-	private Race race;
-	private Class cl;
-	private String gender; 
-	
-	
-	
-	// 2. Physical informations
-	private int size;
-	private int weight;
-	
-	// 3. Characteristics. 
-	
-	private HashMap<Characteristic,Integer> characteristics; 
-	private Skill[] formations;
-	
-	
+    public void takeDammage(int dammage) {
+	this.hp -= this.hp - dammage;
+    }
 	
 
 }
